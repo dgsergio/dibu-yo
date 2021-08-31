212baf7066bf9fe68d34123d8cdc55f76c6a5647
sergio <dg.sergio@gmail.com>
2021-08-31 20:25:17 -0300

    se bug de color del pincel

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const gotero = document.getElementById('gotero');
 const brush = document.getElementById('brush');
 const borrar = document.getElementById('borrar');
 const nuevo = document.getElementById('nuevo');
-let incremento = 3;
-let mouse = { x: undefined, y: undefined};
+let incremento = 4;
 let stop = true;
 let pincel = 20;
 let color = '#000000';
+let colorAnt = '#000000';
 
 canvas.width = window.innerWidth * 0.8;
 canvas.height = window.innerHeight * 0.8;
@@ -19,40 +19,29 @@ canvas.addEventListener('mousedown', ()=>{stop = false});
 canvas.addEventListener('mouseup', ()=>{stop = true});
 canvas.addEventListener('mousemove', e => {
     if (!stop) {
-    mouse.x = e.offsetX;
-    mouse.y = e.offsetY;
-    pintar()
+        const xf = e.offsetX;
+        const yf = e.offsetY;
+        circulo(xf, yf);
     }
 });
-agrandar.addEventListener('click', ()=>{
-    pincel += incremento;
-});
+
+agrandar.addEventListener('click', ()=> pincel += incremento);
 
 achicar.addEventListener('click', ()=>{
     pincel -= incremento; 
     if (pincel <= 1) pincel = 1;
 });
-gotero.addEventListener('change', e => {
-    color = e.target.value;
-})
+gotero.addEventListener('change', e => color = e.target.value);
 borrar.addEventListener('click', () => {
-    color = '#ffffff';
-})
-nuevo.addEventListener('click', e => {
-    ctx.clearRect(0,0,innerWidth,innerHeight);
-})
+    colorAnt = color;
+    color = '#ffffff'
+});
+brush.addEventListener('click', () => color = colorAnt);
+nuevo.addEventListener('click', () => ctx.clearRect(0,0,innerWidth,innerHeight));
 
-function pintar() {
-    ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, pincel , 0, Math.PI*2 , false);
-    ctx.fill();
+function circulo(xf, yf) {
     ctx.fillStyle = color;
-}
-
-function eliminar() {
     ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, pincel , 0, Math.PI*2 , false);
-    ctx.fill();
-    ctx.fillStyle = '#ffffff';
+    ctx.arc(xf, yf, pincel , 0, Math.PI*2 , false);
+    ctx.fill();    
 }
-
